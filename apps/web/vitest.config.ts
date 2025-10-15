2c1ac4465e4fea8fb4d496be33e6d56c4f74afb3
David Murdoch <dsmurdoch357@gmail.com>
2025-10-15 01:27:54 +0100

    chore(unit tests): migrate from jest to vitest

---
+++ apps/web/vitest.config.ts
@@ -0,0 +1,43 @@
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import {
+  coverageConfigDefaults,
+  defaultExclude,
+  defineConfig,
+} from "vitest/config";
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: false,
+    environment: "jsdom",
+    setupFiles: "./src/vitest.setup.ts",
+    exclude: [...defaultExclude, "**/playwright-tests/**"],
+    coverage: {
+      all: true,
+      include: ["src/**/*.[jt]s?(x)"],
+      exclude: [
+        "src/**/*.stories.[jt]s?(x)",
+        "**/playwright-tests/**",
+        ...coverageConfigDefaults.exclude,
+      ],
+      thresholds: {
+        lines: 5,
+        functions: 5,
+        branches: 5,
+        statements: 5,
+      },
+    },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          include: ["src/**/?(*.)+(spec|test).[jt]s?(x)"],
+          exclude: [...defaultExclude, "**/playwright-tests/**"],
+        },
+      },
+    ],
+  },
+});
